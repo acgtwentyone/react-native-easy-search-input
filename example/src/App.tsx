40734825f26f89b0d5e8766cd0f029164d2fa296
Antonio Gonçalves <antonio.goncalves@ideiacv.com>
2022-03-19 18:15:08 -0100

    first commit

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import { Counter } from 'react-native-easy-search-input'
+
+const App = () => {
+  useEffect(() => {
+    console.log('')
+  })
+
+  return <Counter />
+}
+
+export default App
