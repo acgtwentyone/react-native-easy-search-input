0be5ea6ffd29836bf6e2f70100819754e5a01013
Antonio Gonçalves <antonio.goncalves@ideiacv.com>
2022-03-20 18:40:52 -0100

    fixes: initial code added

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,68 @@
 import * as React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 
-export const addOne = (input: number) => input + 1
+interface SearchInputProps {
+  EmptySearchComponent: React.FC
+  ResultsComponent: React.FC
+  searchData: []
+  setSearchData: any
+  searchResults: []
+  setSearchResults: any
+  isSearching: boolean
+  setIsSearching: any
+  searchQuery: string
+  placeholder: string
+  searchContainerStyle: {}
+  searchInputStyle: {}
+  onChangeSearchQuery: any
+}
 
-export const Counter = () => {
-  const [count, setCount] = React.useState(0)
+export const EasySearchInput: React.FC<SearchInputProps> = ({
+  searchData,
+  setSearchData,
+  isSearching,
+  searchResults,
+  setSearchResults,
+  setIsSearching,
+  searchQuery,
+  placeholder,
+  EmptySearchComponent,
+  ResultsComponent,
+  onChangeSearchQuery,
+  searchContainerStyle,
+  searchInputStyle,
+}) => {
+  const _search = async (input: string) => {
+    setIsSearching(true)
+    onChangeSearchQuery(input)
+    const _copyOfData = searchData
+    let results = _copyOfData.filter((data) => {
+      return data
+    })
+    return results
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>You pressed {count} times</Text>
-      <Button onPress={() => setCount(addOne(count))} title='Press Me' />
+    <View style={[styles.container, { ...searchContainerStyle }]}>
+      <TextInput
+        style={[styles.input, { ...searchInputStyle }]}
+        onChangeText={(query) =>
+          _search(query).then((r) => {
+            setSearchResults(r)
+            setIsSearching(false)
+          })
+        }
+        value={searchQuery}
+        placeholder={placeholder ? placeholder : 'search...'}
+      />
+      {!isSearching &&
+        searchResults.length === 0 &&
+        searchQuery !== null &&
+        searchQuery !== '' && <EmptySearchComponent />}
+      {!isSearching &&
+        searchResults.length > 0 &&
+        searchQuery !== null &&
+        searchQuery !== '' && <ResultsComponent />}
     </View>
   )
 }
@@ -20,5 +73,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     height: 200,
+  },
+  input: {
+    padding: 16,
+    borderColor: '#893398',
+    borderWidth: 1,
+    borderRadius: 0,
   },
 })
