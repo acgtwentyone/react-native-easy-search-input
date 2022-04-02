ad3e209758295dba16fe2faa5d75ca2e4cf0b53d
António Gonçalves <acgtwentyone@gmail.com>
2022-04-02 14:07:30 -0100

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { EasySearchInput } from 'react-native-easy-search-input'
+
+import dummyUsers from '../app/dummy/users.json'
 
 const App = () => {
   const [isSearching, setIsSearching] = useState(false)
-  const [searchData, setSearchData] = useState<[]>([{ name: 'Antonio' }])
+  const [searchData, setSearchData] = useState<[]>(dummyUsers)
   const [searchQuery, setSearchQuery] = useState<string>('')
   const [searchResults, setSearchResults] = useState<[]>([])
 
   useEffect(() => {
-    console.log(searchResults)
-    console.log(searchQuery)
+    // console.log(searchResults)
+    // console.log(searchQuery)
   })
 
   const EmptySearchComponent = () => {
-    return <></>
+    return <>No results could be found</>
   }
 
   const ResultsComponent = () => {
-    return <></>
+    return (
+      <>
+        {searchResults.map(({ name }, i) => (
+          <Text key={i}>{name}</Text>
+        ))}
+      </>
+    )
   }
 
   return (
